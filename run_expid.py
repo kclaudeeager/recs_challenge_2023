11db9e07211aaaad577449e3a198990ea471c4dc
Kwizera Claude <claudekwizera003@gmail.com>
2024-11-11 22:34:41 +0200

    Dataset paths

--- run_expid.py
+++ run_expid.py
@@ -1,22 +1,4 @@
-# =========================================================================
-# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========================================================================
-
-
 import os
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import sys
 import logging
 import fuxictr_version
@@ -30,77 +12,82 @@ from fuxictr.preprocess import FeatureProcessor, build_dataset
 import src as model_zoo
 import gc
 import argparse
-import os
 from pathlib import Path
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Set the current working directory to the directory of this script
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+# Print the current working directory and its contents for debugging
+current_directory = os.getcwd()
+print("Current Working Directory:", current_directory)
+print("Contents of Current Directory:", os.listdir(current_directory))
 
+# Get the base directory where the script is located
+base_dir = os.path.dirname(os.path.realpath(__file__))
 
-if __name__ == '__main__':
-    ''' Usage: python run_expid.py --config {config_dir} --expid {experiment_id} --gpu {gpu_device_id}
-    '''
-    # Set the current working directory to the directory of this script
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+# Define data paths based on base_dir
+data_root = os.path.join(base_dir, 'data', 'final')
+train_data_path = os.path.join(data_root, 'train_data_final.csv')
+valid_data_path = os.path.join(data_root, 'valid_data_final.csv')
+test_data_path = os.path.join(data_root, 'test_data_final.csv')
 
-    # Get the base directory where the script is located
-    base_dir = os.path.dirname(os.path.realpath(__file__))
+# Check if data files exist before proceeding
+if not all(os.path.exists(f) for f in [train_data_path, valid_data_path, test_data_path]):
+    print("One or more data files do not exist.")
+    print(f"Train Data Exists: {os.path.exists(train_data_path)}")
+    print(f"Valid Data Exists: {os.path.exists(valid_data_path)}")
+    print(f"Test Data Exists: {os.path.exists(test_data_path)}")
+    sys.exit(1)  # Exit if files are missing
 
-    # Define data paths based on base_dir
-    data_root = os.path.join(base_dir, 'data', 'final')
-    train_data_path = os.path.join(data_root, 'train_data_final.csv')
-    valid_data_path = os.path.join(data_root, 'valid_data_final.csv')
-    test_data_path = os.path.join(data_root, 'test_data_final.csv')
+# Load configuration
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='./config/', help='The config directory.')
+parser.add_argument('--expid', type=str, default='DeepFM_test', help='The experiment id to run.')
+parser.add_argument('--gpu', type=int, default=-1, help='The gpu index, -1 for cpu')
+args = vars(parser.parse_args())
 
-    # Load configuration
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='./config/', help='The config directory.')
-    parser.add_argument('--expid', type=str, default='DeepFM_test', help='The experiment id to run.')
-    parser.add_argument('--gpu', type=int, default=-1, help='The gpu index, -1 for cpu')
-    args = vars(parser.parse_args())
+experiment_id = args['expid']
+params = load_config(args['config'], experiment_id)
+params['gpu'] = args['gpu']
 
-    experiment_id = args['expid']
-    params = load_config(args['config'], experiment_id)
-    params['gpu'] = args['gpu']
+# Update params with absolute paths
+params['data_root'] = data_root
+params['train_data'] = train_data_path
+params['valid_data'] = valid_data_path
+params['test_data'] = test_data_path
 
-    # Update params with absolute paths
-    params['data_root'] = data_root
-    params['train_data'] = train_data_path
-    params['valid_data'] = valid_data_path
-    params['test_data'] = test_data_path
+set_logger(params)
+logging.info("Params: " + print_to_json(params))
+seed_everything(seed=params['seed'])
 
-    set_logger(params)
-    logging.info("Params: " + print_to_json(params))
-    seed_everything(seed=params['seed'])
+data_dir = os.path.join(params['data_root'], params['dataset_id'])
+feature_map_json = os.path.join(data_dir, "feature_map.json")
 
-    data_dir = os.path.join(params['data_root'], params['dataset_id'])
-    feature_map_json = os.path.join(data_dir, "feature_map.json")
-    if params["data_format"] == "csv":
-        # Build feature_map and transform h5 data
-        feature_encoder = FeatureProcessor(**params)
-        params["train_data"], params["valid_data"], params["test_data"] = \
-            build_dataset(feature_encoder, **params)
-    feature_map = FeatureMap(params['dataset_id'], data_dir)
-    feature_map.load(feature_map_json, params)
-    logging.info("Feature specs: " + print_to_json(feature_map.features))
-    
-    model_class = getattr(model_zoo, params['model'])
-    model = model_class(feature_map, **params)
-    model.count_parameters() # print number of parameters used in model
+if params["data_format"] == "csv":
+    # Build feature_map and transform h5 data
+    feature_encoder = FeatureProcessor(**params)
+    params["train_data"], params["valid_data"], params["test_data"] = \
+        build_dataset(feature_encoder, **params)
 
-    train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
-    model.fit(train_gen, validation_data=valid_gen, **params)
+feature_map = FeatureMap(params['dataset_id'], data_dir)
+feature_map.load(feature_map_json, params)
+logging.info("Feature specs: " + print_to_json(feature_map.features))
 
-    logging.info('****** Validation evaluation ******')
-    valid_result = model.evaluate(valid_gen)
-    del train_gen, valid_gen
-    gc.collect()
-    
-    
-    result_filename = Path(args['config']).name.replace(".yaml", "") + '.csv'
-    with open(result_filename, 'a+') as fw:
-        fw.write(' {},[command] python {},[exp_id] {},[dataset_id] {},[train] {},[val] {} \n' \
-            .format(datetime.now().strftime('%Y%m%d-%H%M%S'), 
-                    ' '.join(sys.argv), experiment_id, params['dataset_id'],
-                    "N.A.", print_to_list(valid_result)))
+model_class = getattr(model_zoo, params['model'])
+model = model_class(feature_map, **params)
+model.count_parameters()  # Print number of parameters used in model
 
+train_gen, valid_gen = H5DataLoader(feature_map, stage='train', **params).make_iterator()
+model.fit(train_gen, validation_data=valid_gen, **params)
+
+logging.info('****** Validation evaluation ******')
+valid_result = model.evaluate(valid_gen)
+del train_gen, valid_gen
+gc.collect()
+
+result_filename = Path(args['config']).name.replace(".yaml", "") + '.csv'
+with open(result_filename, 'a+') as fw:
+    fw.write(' {},[command] python {},[exp_id] {},[dataset_id] {},[train] {},[val] {} \n' \
+        .format(datetime.now().strftime('%Y%m%d-%H%M%S'), 
+                ' '.join(sys.argv), experiment_id, params['dataset_id'],
+                "N.A.", print_to_list(valid_result)))
